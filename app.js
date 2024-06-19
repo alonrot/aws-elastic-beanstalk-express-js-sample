91d0cbe8e7fc91c06ba362e3e40e878cd835c06b
Alonso Marco-Valle (remote) <alonso.marcovalle@figure.ai>
2024-06-19 13:18:43 -0700

    debug

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+// const express = require('express');
+// const app = express();
+// const port = 8080;
+// const { spawn } = require('child_process');
+
+// app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/public/index.html');
+// });
+
+// app.get('/chatbot', (req, res) => {
+//   // const pythonProcess = spawn('python', [`${__dirname}/chatbot_script.py`]);
+//   const pythonProcess = spawn('python', [`${__dirname}/chat_gpt_audio/test_chat.py`]);
+
+//   pythonProcess.stdout.on('data', (data) => {
+//     res.send(data.toString());
+//   });
+
+//   pythonProcess.stderr.on('data', (data) => {
+//     console.error(`stderr: ${data}`);
+//   });
+
+//   pythonProcess.on('close', (code) => {
+//     console.log(`child process exited with code ${code}`);
+//   });
+// });
+
+// app.listen(port, () => {
+//   console.log(`App running on http://localhost:${port}`);
+// });
+
+
 const express = require('express');
 const app = express();
 const port = 8080;
@@ -10,8 +43,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/chatbot', (req, res) => {
-  // const pythonProcess = spawn('python', [`${__dirname}/chatbot_script.py`]);
-  const pythonProcess = spawn('python', [`${__dirname}/chat_gpt_audio/test_chat.py`]);
+  const pythonProcess = spawn('python', [`${__dirname}/test_chat.py`]);
 
   pythonProcess.stdout.on('data', (data) => {
     res.send(data.toString());
---
+++ test_chat.py
@@ -0,0 +1,11 @@
+import time
+
+def main():
+    response = "Hello, this is a simple chatbot response!"
+    print(response)
+    # while True:
+    #     time.sleep(1)
+    #     print("Sleeping ...")
+
+if __name__ == "__main__":
+    main()
